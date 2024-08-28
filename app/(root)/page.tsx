096235e8264f6268b85c8ecbfc97b329f9894b44
Siddharth <preparation013@gmail.com>
2024-08-28 15:11:44 +0530

    init

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+    return ( <>
+        <p>Admin dashboard</p>
+        <Button size={"default"}> Click me</Button>
+        </>
+    )
+}
